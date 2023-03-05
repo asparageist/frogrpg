0bb0a30ad15c875eb719f99bd481262b1ba446c1
Joseph Murray <josephwmurray@gmail.com>
2023-03-05 15:25:15 -0800

    create a character object with a name

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -1,8 +1,14 @@
-import Template from '../src/js/character.js';
+import Character from '../src/js/character.js';
 
 describe('Character', () => {
-  
-  test('should correctly create a test that starts with "should"', () => {
-    //code
+  let character;
+
+  beforeEach(() => {
+    character = new Character("biff", "croakinsen");
+  });
+
+  test('should create a character object with a name', () => {
+    expect(character.name1).toEqual("biff");
+    expect(character.name2).toEqual("croakinsen");
   });
 });
--- src/js/character.js
+++ src/js/character.js
@@ -1,3 +1,6 @@
 export default class Character {
-  
+  constructor(name1, name2) {
+    this.name1 = name1;
+    this.name2 = name2;
+  }
 }
