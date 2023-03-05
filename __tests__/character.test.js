956c1d66af156a7376a3340f8f008975509e942b
Joseph Murray <josephwmurray@gmail.com>
2023-03-05 15:47:00 -0800

    assign a class based on stats

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -4,11 +4,23 @@ describe('Character', () => {
   let character;
 
   beforeEach(() => {
-    character = new Character("biff", "croakinsen");
+    character = new Character("biff", "croakinsen",5,1,1,2);
   });
 
   test('should create a character object with a name', () => {
     expect(character.name1).toEqual("biff");
     expect(character.name2).toEqual("croakinsen");
   });
+
+  test('should give attributes to the character', () => {
+    expect(character.strStat).toEqual(5);
+    expect(character.spdStat).toEqual(1);
+    expect(character.intStat).toEqual(1);
+    expect(character.endStat).toEqual(2);
+  });
+
+  test('should assign class from given stats', () => {
+    expect(character.getClass()).toEqual("berserker");
+  });
+
 });
--- src/js/character.js
+++ src/js/character.js
@@ -1,6 +1,24 @@
 export default class Character {
-  constructor(name1, name2) {
+  constructor(name1, name2, strStat, spdStat, intStat, endStat) {
     this.name1 = name1;
     this.name2 = name2;
+    this.strStat = strStat;
+    this.spdStat = spdStat;
+    this.intStat = intStat;
+    this.endStat = endStat;
+  }
+
+  getClass() {
+    if (this.strStat >= 4) {
+      return "berserker";
+    } else if (this.spdStat >= 4) {
+      return "ninja";
+    } else if (this.intStat >= 4) {
+      return "oracle";
+    } else if (this.endStat >= 4) {
+      return "juggernaut";
+    } else {
+      return "normal";
+    }
   }
 }
